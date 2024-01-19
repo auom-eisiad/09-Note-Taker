a6f631a2467ca6e8ffe2f68eeff0232936e9e2d0
auom.eisiad <daisiemouaa@gmail.com>
2024-01-19 11:39:32 -0500

    trying to figure out the problem in console and fixed deleyte

--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,20 @@
 const app = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
-const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtils");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
 
-// GET Route for retrieving all note_id
+// GET Route for retrieving all notes
 app.get("/", (req, res) => {
-  console.info(`${req.method} note_id saved`);
+  console.info(`${req.method} notes saved`);
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route adding new note
 app.post("/", (req, res) => {
-  console.info(`${req.method} request received to add a Note`);
+  console.info(`${req.method} request received to add a note`);
   console.log(req.body);
 
   const { title, text } = req.body;
@@ -37,19 +41,29 @@ app.post("/", (req, res) => {
 app.delete("/:note_id", (req, res) => {
   let noteId = req.params.note_id;
 
-  console.log(noteId);
+  // Log the id that is being identified to delete
+  console.log("Received DELETE request with noteId:", noteId);
 
   readFromFile("./db/db.json")
     .then((data) => JSON.parse(data))
     .then((json) => {
+      console.log("Existing data:", json);
+
       // Make a new array of all db except the one with the ID provided in the URL
-      const result = json.filter((note_id) => note_id.note_id !== noteId);
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // Log what happens after being deleted
+      console.log("Result after filtering:", result);
 
       // Save that array to the filesystem
       writeToFile("./db/db.json", result);
 
       // Respond to the DELETE request
       res.json(`Item ${noteId} has been deleted 🗑️`);
+    })
+    .catch((error) => {
+      console.error("Error processing DELETE request:", error);
+      res.status(500).json({ error: "Internal Server Error" });
     });
 });
 
